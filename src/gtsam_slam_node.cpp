12c913cfd8f730f0771d8e323ab07bfcb16164df
Yongkie Wiyogo <4080600+ywiyogo@users.noreply.github.com>
2026-04-10 17:52:14 +0800

    Remove tf2_geometry_msgs from gtsam node

--- src/gtsam_slam_node.cpp
+++ src/gtsam_slam_node.cpp
@@ -48,8 +48,6 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/fluid_pressure.hpp>
 #include <sensor_msgs/msg/imu.hpp>
-#include <tf2/convert.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 #include <tf2_ros/transform_broadcaster.h>
 
 #include "portable_slam/gtsam_slam.hpp"
